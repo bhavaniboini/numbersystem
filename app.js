f172ff3657319401d2add83c1e9f6a6b6034c747
Bhavani Boini <137797521+bhavaniboini@users.noreply.github.com>
2024-12-24 20:26:42 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const numbers = [
+    19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30,
+  ];
+function isPrime(num) {
+    if (num <=1) return false
+    for (let i =2; i<= Math.sqrt(num);i++) {
+        if (num%i===0) return false;
+    }
+    return true;
+}  
+const primenumbers= numbers.filter(isPrime);
+console.log("prime numbers:",primenumbers);
